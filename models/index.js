6b56711e8837ae66fb7fcd8845f20617e81896e2
dylanstoudt <138310076+dylanstoudt@users.noreply.github.com>
2023-09-06 09:55:01 -0400

    added starter

--- models/index.js
+++ models/index.js
@@ -1,34 +1,13 @@
-//Import models
 const User = require('./User');
 const Post = require('./Post');
-const Comment = require('./Comment')
 
-// Relationship between Users and Posts
 User.hasMany(Post, {
-    foreignKey: 'user_id'
-})
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
 
 Post.belongsTo(User, {
-    foreignKey: 'user_id'
-})
+  foreignKey: 'user_id'
+});
 
-// Relationship between Users and Comments
-User.hasMany(Comment, {
-    foreignKey: 'user_id'
-})
-
-Comment.belongsTo(User, {
-    foreignKey: 'user_id'
-})
-
-// Relationship between Post and Comment
-Post.hasMany(Comment,{
-    foreignKey: 'post_id'
-
-})
-
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
-})
-
-module.exports = { User, Post, Comment }
+module.exports = { User, Post };
